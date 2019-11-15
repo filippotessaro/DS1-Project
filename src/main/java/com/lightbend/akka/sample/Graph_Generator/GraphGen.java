cd35dff40b66b9e3664c4e4a4f9bc0632246f5d0
framatho <frank.17.rocky@gmail.com>
2019-11-15 17:18:07 +0100

    Created Random Tree Generator

---
+++ src/main/java/com/lightbend/akka/sample/Graph_Generator/GraphGen.java
@@ -0,0 +1,110 @@
+package com.lightbend.akka.sample.Graph_Generator;
+
+public class
+GraphGen {
+
+    private static final class Edge implements Comparable<Edge> {
+        private int v;
+        private int w;
+
+        private Edge(int v, int w) {
+            if (v < w) {
+                this.v = v;
+                this.w = w;
+            }
+            else {
+                this.v = w;
+                this.w = v;
+            }
+        }
+
+        public int compareTo(Edge that) {
+            if (this.v < that.v) return -1;
+            if (this.v > that.v) return +1;
+            if (this.w < that.w) return -1;
+            if (this.w > that.w) return +1;
+            return 0;
+        }
+    }
+
+    // this class cannot be instantiated
+    private GraphGen() { }
+
+    public static Graph regular(int V, int k) {
+        if (V*k % 2 != 0) throw new IllegalArgumentException("Number of vertices * k must be even");
+        Graph G = new Graph(V);
+
+        // create k copies of each vertex
+        int[] vertices = new int[V*k];
+        for (int v = 0; v < V; v++) {
+            for (int j = 0; j < k; j++) {
+                vertices[v + V*j] = v;
+            }
+        }
+
+        // pick a random perfect matching
+        StdRandom.shuffle(vertices);
+        for (int i = 0; i < V*k/2; i++) {
+            G.addEdge(vertices[2*i], vertices[2*i + 1]);
+        }
+        return G;
+    }
+
+    /**
+     * Returns a uniformly random tree on {V} vertices.
+     * This algorithm uses a Prufer sequence and takes time proportional to V*log V.
+     * @param V the number of vertices in the tree
+     * @return a uniformly random tree on {V} vertices
+     */
+    public static Graph tree(int V) {
+        Graph G = new Graph(V);
+
+        // special case
+        if (V == 1) return G;
+
+        // Cayley's theorem: there are V^(V-2) labeled trees on V vertices
+        // Prufer sequence: sequence of V-2 values between 0 and V-1
+        // Prufer's proof of Cayley's theorem: Prufer sequences are in 1-1
+        // with labeled trees on V vertices
+        int[] prufer = new int[V-2];
+        for (int i = 0; i < V-2; i++)
+            prufer[i] = StdRandom.uniform(V);
+
+        // degree of vertex v = 1 + number of times it appers in Prufer sequence
+        int[] degree = new int[V];
+        for (int v = 0; v < V; v++)
+            degree[v] = 1;
+        for (int i = 0; i < V-2; i++)
+            degree[prufer[i]]++;
+
+        // pq contains all vertices of degree 1
+        MinPQ<Integer> pq = new MinPQ<Integer>();
+        for (int v = 0; v < V; v++)
+            if (degree[v] == 1) pq.insert(v);
+
+        // repeatedly delMin() degree 1 vertex that has the minimum index
+        for (int i = 0; i < V-2; i++) {
+            int v = pq.delMin();
+            G.addEdge(v, prufer[i]);
+            degree[v]--;
+            degree[prufer[i]]--;
+            if (degree[prufer[i]] == 1) pq.insert(prufer[i]);
+        }
+        G.addEdge(pq.delMin(), pq.delMin());
+        return G;
+    }
+
+    public static void main(String[] args) {
+        int V = Integer.parseInt(args[0]);
+        int E = Integer.parseInt(args[1]);
+        int V1 = V/2;
+        int V2 = V - V1;
+
+        StdOut.println("tree");
+        StdOut.println(tree(V));
+        StdOut.println();
+    }
+
+}
+
+
