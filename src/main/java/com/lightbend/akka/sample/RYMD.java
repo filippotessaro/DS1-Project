93b77d9deef126e842e9ed76c2054e6de3bc3910
framatho <frank.17.rocky@gmail.com>
2019-11-15 17:21:43 +0100

    Implemented random tree generator in the main

--- src/main/java/com/lightbend/akka/sample/RYMD.java
+++ src/main/java/com/lightbend/akka/sample/RYMD.java
@@ -1,7 +1,8 @@
 package com.lightbend.akka.sample;
 
-import java.io.IOException;
-
+import akka.actor.Actor;
+import com.lightbend.akka.sample.Graph_Generator.Graph;
+import com.lightbend.akka.sample.Graph_Generator.StdOut;
 import com.lightbend.akka.sample.Messages.Message;
 import com.lightbend.akka.sample.Messages.Message.Building_tree;
 import com.lightbend.akka.sample.Messages.Message.Initialize;
@@ -10,61 +11,81 @@ import com.lightbend.akka.sample.Messages.Message.NodeFailure;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 
+import static com.lightbend.akka.sample.Graph_Generator.GraphGen.tree;
+
+
 public class RYMD {
+
+	final static ActorSystem system = ActorSystem.create("RaymondAlgorithm");
+
+	public static ActorRef OnceUponATime(int vertex) {
+		final ActorRef nodeRef =
+				system.actorOf(Node.props(vertex));
+		return nodeRef;
+	}
+
 	 public static void main(String[] args) {
-		    final ActorSystem system = ActorSystem.create("RaymondAlgorithm");
 		    try {
-		      //#create-actors
-		      final ActorRef Node_A = 
-		        system.actorOf(Node.props(1),"A");
-		      final ActorRef Node_B = 
-				        system.actorOf(Node.props(2),"B");
-		      final ActorRef Node_C = 
-				        system.actorOf(Node.props(3),"C");
-		      final ActorRef Node_D = 
-				        system.actorOf(Node.props(4),"D");
-		      final ActorRef Node_E = 
-				        system.actorOf(Node.props(5),"E");
-		      //#create-actors
-	        
-		      //#create tree-structure 
-		      Node_A.tell(new Building_tree(2,Node_B), ActorRef.noSender());
-		      Node_A.tell(new Building_tree(3,Node_C), ActorRef.noSender());
-		    
-		      Node_C.tell(new Building_tree(1,Node_A), ActorRef.noSender());
-		      Node_C.tell(new Building_tree(5,Node_E), ActorRef.noSender());
-		      Node_C.tell(new Building_tree(4,Node_D), ActorRef.noSender());
-		    
-		      Node_B.tell(new Building_tree(1,Node_A), ActorRef.noSender());
-		    
-		      Node_E.tell(new Building_tree(3,Node_C), ActorRef.noSender());
-		      
-		      Node_D.tell(new Building_tree(3,Node_C), ActorRef.noSender());
-		      //#create tree-structure
-		    
-		      //#inject token in arbitrary node and flood it
-		      Node_A.tell(new Initialize(1), ActorRef.noSender());
-		      //#inject token in arbitrary node and flood it
+				int V = Integer.parseInt(args[0]);
+				int E = Integer.parseInt(args[1]);
+				int V1 = V/2;
+				int V2 = V - V1;
+				ActorRef[] arr = new ActorRef[V];
 
-			  System.out.println(">>> Press f to fail a node <<<");
+				//StdOut.println(tree(V));
+				//StdOut.println();
 
-			  Scanner s = new Scanner(System.in);
-			  char command = s.next().charAt(0);
 
-			  if (command == 'f') {
-			  		ThreadLocalRandom current = ThreadLocalRandom.current();
-					int rand = current.nextInt(5) + 1;
-					Node_C.tell(new NodeFailure(), ActorRef.noSender());
+				StdOut.println("The random tree is:");
+				//Create the random tree
+				Graph G = tree(V);
+				StdOut.println(G);
+
+				//Create actors
+				for (int i = 0; i < V; i++) {
+					arr[i] = OnceUponATime(i);
+				}
+				for (int j = 0; j < arr.length; j++) {
+					Iterable<Integer> neighbors = G.adj(j);
+					for (int vertex: neighbors) {
+						arr[j].tell(new Building_tree(vertex,arr[vertex]), ActorRef.noSender());
+					}
+				}
+				//Create actors
+
+				//#inject token in a random node and flood it
+				Random rand = new Random();
+				int value = rand.nextInt(V);
+				System.out.println("The starting holder is: " + value);
+				arr[value].tell(new Initialize(value), ActorRef.noSender());
+				//#inject token in arbitrary node and flood it
+
+				/*
+				Scanner s = new Scanner(System.in);
+				char command = s.next().charAt(0);
+			
+				if (command == 'f') {
+					Random rand_om = new Random();
+					int valu_e = rand_om.nextInt(V);
+					arr[valu_e].tell(new Message.NodeFailure(), ActorRef.noSender());
 				}
 
-		    } catch (Exception e) {
+				System.out.println(">>> Press f to fail a node <<<");
+*/
+                System.out.println(">>> Press ENTER to exit <<<");
+                System.in.read();
+		    } catch (IOException ioe) {
 		    } finally {
-				//system.terminate();
-				System.out.println("Stopped");
+				system.terminate();
 	    }
 	}
 }
